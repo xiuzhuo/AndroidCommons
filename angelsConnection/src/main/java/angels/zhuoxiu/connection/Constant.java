ba8ebbf8adab1d46eb977efa26961468da9a89eb
Zhuo Xiu <xiuzhuo@outlook.com>
2015-02-26 14:48:49 +0100

    add

---
+++ angelsConnection/src/main/java/angels/zhuoxiu/connection/Constant.java
@@ -0,0 +1,9 @@
+package angels.zhuoxiu.connection;
+
+public interface Constant {
+	public static final String HTTP = "http";
+	public static final String HTTPS = "https";
+	public static final String AUTHORIZATION = "Authorization";
+	public static final String BEARER = "Bearer";
+	
+}
